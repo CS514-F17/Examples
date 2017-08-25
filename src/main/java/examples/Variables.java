57f327f318fc1bf5736f6ddd508a00600c2ad35b
Sami Rollins <srollins@cs.usfca.edu>
2017-08-25 12:42:45 -0700

    Adding examples from 8/25.

--- src/main/java/examples/Variables.java
+++ src/main/java/examples/Variables.java
@@ -3,8 +3,19 @@ package examples;
 public class Variables {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int x = 5;
+		int y = 2;
+		double result = ((double) x) / y;
 
+		String s1 = "hello";
+		String s2;
+		s2 = new String("hello");
+		s2 = s2.replace('l', 'X');
+		System.out.println(s2);
+		
+		
+		
+		
 	}
 
 }
