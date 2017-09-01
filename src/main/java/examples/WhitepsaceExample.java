05dfd335c0b2299b4b30fb2cb1259cef06dbc55d
Sami Rollins <srollins@cs.usfca.edu>
2017-09-01 09:49:07 -0700

    add whitespace example

---
+++ src/main/java/examples/WhitepsaceExample.java
@@ -0,0 +1,43 @@
+package examples;
+
+public class WhitepsaceExample {
+
+	private boolean isValid(String input) {
+		if(input.equals("abc") || input.equals("def") ||
+				input.equals("ghi")) {
+			return true;
+		}
+		return false;
+	}
+	
+	public int convert(String input1, String input2) {
+		
+		//covnert inputs to a number
+		//input must be abc or def
+		
+		if(!isValid(input1) || !isValid(input2)) {
+			return -1;
+		}
+		else {
+			//return conversion
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		String test = "hello\tthere class";
+		int count = 0;
+		
+		for(int i = 0; i < test.length(); i++) {
+			char c = test.charAt(i);
+			if(Character.isWhitespace(c)) {
+				count++;
+			}
+		}
+		System.out.println("Number of whitespace characters in the string - " + test + " - is " + count); 
+	
+		
+	}
+
+}
